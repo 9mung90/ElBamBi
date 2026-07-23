3ed937c57091d2d0f11cd9d17958d4e2cb66aca7
9mung90 <koujaehong39@gmail.com>
2026-07-23 16:01:51 +0900

    refactor: extract ListTop category icon mapping

---
+++ src/pages/listTop/categoryIcons.ts
@@ -0,0 +1,35 @@
+import ashTopIcon from '../../assets/images/top_icon/ash.webp';
+import bossTopIcon from '../../assets/images/top_icon/boss.webp';
+import buildTopIcon from '../../assets/images/top_icon/imgi_6_152.webp';
+import characterTopIcon from '../../assets/images/top_icon/character.webp';
+import dealTopIcon from '../../assets/images/top_icon/deal.webp';
+import etcTopIcon from '../../assets/images/top_icon/etc.webp';
+import gestureTopIcon from '../../assets/images/top_icon/gesture.webp';
+import mapTopIcon from '../../assets/images/top_icon/map.webp';
+import optionMakeTopIcon from '../../assets/images/top_icon/optin_make.webp';
+import optionTopIcon from '../../assets/images/top_icon/option.webp';
+import relicTopIcon from '../../assets/images/top_icon/relic.webp';
+import saveTopIcon from '../../assets/images/top_icon/save.webp';
+import spellTopIcon from '../../assets/images/top_icon/ee.webp';
+import talismanTopIcon from '../../assets/images/top_icon/talisman.webp';
+import vesselTopIcon from '../../assets/images/top_icon/vessel.webp';
+import weaponTopIcon from '../../assets/images/top_icon/weapone.webp';
+
+export const categoryIconAssets: Record<string, string> = {
+  ashes: ashTopIcon,
+  bosses: bossTopIcon,
+  builds: buildTopIcon,
+  characters: characterTopIcon,
+  gestures: gestureTopIcon,
+  items: etcTopIcon,
+  map: mapTopIcon,
+  options: optionTopIcon,
+  relics: relicTopIcon,
+  'relic-builder': optionMakeTopIcon,
+  'save-parser': saveTopIcon,
+  spells: spellTopIcon,
+  'stats-calculator': dealTopIcon,
+  talismans: talismanTopIcon,
+  vessels: vesselTopIcon,
+  weapons: weaponTopIcon,
+};
